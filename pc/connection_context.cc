844826fa4de9d8ad83032a2423743c34138bfce6
UI-RayanWang <rayan.wang@ui.com>
2022-09-22 11:31:50 +0800

    Add signaling thread itself to the allow invoked thread list.
    (cherry picked from commit 3b997b56f3e51317ad22f0e918f98a34848bbc5b)

--- pc/connection_context.cc
+++ pc/connection_context.cc
@@ -114,6 +114,9 @@ ConnectionContext::ConnectionContext(
 
   signaling_thread_->AllowInvokesToThread(worker_thread());
   signaling_thread_->AllowInvokesToThread(network_thread_);
+#if !defined(NDEBUG)
+  signaling_thread_->AllowInvokesToThread(signaling_thread_);
+#endif
   worker_thread_->AllowInvokesToThread(network_thread_);
   if (!network_thread_->IsCurrent()) {
     // network_thread_->IsCurrent() == true means signaling_thread_ is
