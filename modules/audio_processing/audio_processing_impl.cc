df9ac3fa1463f52d157f3bbd5221cb303ef02894
UI-RayanWang <rayan.wang@ui.com>
2023-10-26 21:47:33 +0800

    Return the original rms level calculated by Google.

--- modules/audio_processing/audio_processing_impl.cc
+++ modules/audio_processing/audio_processing_impl.cc
@@ -1534,9 +1534,14 @@ int AudioProcessingImpl::ProcessCaptureStreamLocked() {
       capture_buffer->channels_const()[0],
       capture_nonlocked_.capture_processing_format.num_frames()));
 // UI Customization Begin
-  RmsLevel::Levels levels = capture_input_rms_.AverageAndPeak();
-  if (submodules_.capture_post_processor) {
-    submodules_.capture_post_processor->UpdateAudioIndicator(levels.average, levels.peak);
+  const bool update_rms = ++capture_rms_update_interval_counter_ >= 10;
+  RmsLevel::Levels levels;
+  if (update_rms) {
+    capture_rms_update_interval_counter_ = 0;
+    levels = capture_input_rms_.AverageAndPeak();
+    if (submodules_.capture_post_processor) {
+      submodules_.capture_post_processor->UpdateAudioIndicator(-levels.average + 0.5, -levels.peak + 0.5);
+    }
   }
 // UI Customization End
   const bool log_rms = ++capture_rms_interval_counter_ >= 1000;
@@ -1544,6 +1549,8 @@ int AudioProcessingImpl::ProcessCaptureStreamLocked() {
     capture_rms_interval_counter_ = 0;
 // UI Customization Begin
     // RmsLevel::Levels levels = capture_input_rms_.AverageAndPeak();
+    if (!update_rms)
+      levels = capture_input_rms_.AverageAndPeak();
 // UI Customization End
     RTC_HISTOGRAM_COUNTS_LINEAR("WebRTC.Audio.ApmCaptureInputLevelAverageRms",
                                 levels.average, 1, RmsLevel::kMinLevelDb, 64);
